fbedfc8b198091368c7ea435d5d49884994e5390
Yucheng‘s Mac <Yucheng@DN0a1e4e40.SUNet>
2017-10-03 07:43:40 -0700

    direct

--- rd_update/views_descend.py
+++ rd_update/views_descend.py
@@ -12,31 +12,21 @@ class IRB(Page):
 	def is_displayed(self):
 		return self.round_number == 1
 
-class Prior(Page):
-	form_model = models.Player
-	form_fields = ['prior']
-
-	def is_displayed(self):
-		return self.round_number == 1
-
-	def vars_for_template(self):
-		return {
-			'prior_g': Constants.treatment_dict['good_prior'][self.player.treatment],
-		}
 
 class Instructions(Page):
 	form_model = models.Player
+
 	def get_form_fields(self):
 		if self.player.expectation != 0:
-			return ['truefalse3', 'truefalse5', 'multiple1', 'blank2', 'blank3']
+			return ['truefalse1', 'truefalse2', 'truefalse3', 'truefalse4', 'multiple1', 'blank1', 'blank2', 'blank3']
 		elif self.player.high_acc != self.player.low_acc:
-			return ['truefalse3', 'truefalse5', 'blank2', 'blank3']
+			return ['truefalse1', 'truefalse2', 'truefalse3', 'truefalse4', 'blank1', 'blank2', 'blank3']
 		else:
-			return ['truefalse5', 'blank3']
+			return ['truefalse1', 'truefalse2', 'truefalse4', 'blank1', 'blank3']
 
 	def is_displayed(self):
 		return self.round_number == 1
-	
+
 	def vars_for_template(self):
 		return {
 			'prior_g': Constants.treatment_dict['good_prior'][self.player.treatment],
@@ -49,19 +39,26 @@ class Instructions(Page):
 			'junior_prob': 100 - Constants.treatment_dict['senior_prob'][self.player.treatment],
 			'expectation': self.player.expectation,
 		}
+
 	def multiple1_choices(self):
-		return [[1, self.player.senior_name], [-1, self.player.junior_name] ]
+		return [[1, self.player.senior_name], [-1, self.player.junior_name]]
 
 	def error_message(self, values):
 		if self.player.participant.vars.get('failure') == 0:
 			self.player.failures = 0
 
 		summand = 0
+		if values["truefalse1"] != True:
+			summand += 1
+		if values["truefalse2"] != True:
+			summand += 1
 		if self.player.high_acc != self.player.low_acc and values["truefalse3"] != True:
 			summand += 1
-		if values["truefalse5"] != True:
+		if values["truefalse4"] != True:
 			summand += 1
-		if self.player.expectation != 0 and values["multiple1"] != (self.player.senior_prob >= 0.5) - (self.player.senior_prob <= 0.5):
+		if self.player.expectation != 0 and values["multiple1"] != self.player.expectation:
+			summand += 1
+		if values["blank1"] != Constants.treatment_dict['good_prior'][self.player.treatment]:
 			summand += 1
 		if self.player.high_acc != self.player.low_acc and values["blank2"] != Constants.treatment_dict['high_acc'][self.player.treatment]:
 			summand += 1
@@ -73,9 +70,8 @@ class Instructions(Page):
 			return 'Sorry, you got ' + str(summand) + " questions wrong."
 		elif summand == 1 and self.player.participant.vars.get('failure') < Constants.failuretolerance:
 			self.player.participant.vars['failure'] = 1 + self.player.participant.vars.get('failure')
-			self.player.failures = self.player.failures  + 1
+			self.player.failures = self.player.failures + 1
 			return 'Almost there! You just got one question wrong!'
-
 
 class sorrybutton2(Page):
 	form_model = models.Player
@@ -103,7 +99,7 @@ class Task(Page):
 			'senior': self.player.senior,
 			'signal': self.player.signal,
 			'lottery_odds': self.player.lottery_odds,
-			'right_side_odds': Constants.right_side_odds[::-1],
+			'right_side_odds': Constants.right_side_odds,
 			'right_side_start': Constants.right_side_odds[0],
 			'expectation': self.player.expectation,
 		}
@@ -137,7 +133,6 @@ class Results(Page):
 page_sequence = [
 	Welcome,
 	IRB,
-	Prior,
 	Instructions,
 	sorrybutton2,
 	Task,
